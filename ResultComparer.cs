b957afe3fec0f8cd40548df2993fb9cc64f9080b
Jlobblet <jlobblet-github@gmx.co.uk>
2021-02-19 15:30:33 +0000

    Use expression bodies for shorter code

--- ResultComparer.cs
+++ ResultComparer.cs
@@ -5,14 +5,8 @@ namespace Bot600
 {
     public class ResultComparer<T> : IEqualityComparer<Result<T>>
     {
-        public bool Equals(Result<T> x, Result<T> y)
-        {
-            return x == y;
-        }
+        public bool Equals(Result<T> x, Result<T> y) => x == y;
 
-        public int GetHashCode(Result<T> obj)
-        {
-            return obj.GetHashCode();
-        }
+        public int GetHashCode(Result<T> obj) => obj.GetHashCode();
     }
 }
