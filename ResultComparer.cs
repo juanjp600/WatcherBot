98f39950d2bd84a5f267a6dcc1cc84bd4f4cca8a
Jlobblet <jlobblet-github@gmx.co.uk>
2021-02-24 19:20:05 +0000

    Actually really remove duplicate hashes this time

--- ResultComparer.cs
+++ ResultComparer.cs
@@ -1,12 +1,27 @@
-using System;
 using System.Collections.Generic;
 
 namespace Bot600
 {
-    public class ResultComparer<T> : IEqualityComparer<Result<T>>
+    public class HashComparer : IEqualityComparer<Result<string>>
     {
-        public bool Equals(Result<T> x, Result<T> y) => x == y;
+        public bool Equals(Result<string> x, Result<string> y)
+        {
+            if (x is null && y is null)
+                return true;
+            if (x is null || y is null)
+                return false;
+            if (x.IsSuccess != y.IsSuccess)
+                return false;
+            if (!x.IsSuccess)
+                return x.FailureMessage == y.FailureMessage;
+            return x.Value.StartsWith(y.Value) || y.Value.StartsWith(x.Value);
+        }
 
-        public int GetHashCode(Result<T> obj) => obj.GetHashCode();
+        public int GetHashCode(Result<string> obj)
+        {
+            var baseHash = obj.IsSuccess ? obj.Value.Substring(0, 5).GetHashCode() : obj.FailureMessage.GetHashCode();
+            var lsb = obj.IsSuccess ? 1 : 0;
+            return (baseHash << 1) | lsb;
+        }
     }
 }
