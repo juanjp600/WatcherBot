819da908c1630789375464826c5b04228c52fd07
Juan Pablo Arce <juanjprot@gmail.com>
2021-04-01 12:38:08 -0300

    Wholesome command :)

---
+++ Commands/Hyena.cs
@@ -0,0 +1,22 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot600.Commands
+{
+    public class HyenaCommandModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("hyena", RunMode = RunMode.Async)]
+        [Summary("smh my head")]
+        [Alias("yeen")]
+        public async Task Hyena()
+        {
+            Console.WriteLine("yeen command");
+            var author = Context.Message.Author;
+            var dm = await author.GetOrCreateDMChannelAsync();
+            await dm.SendMessageAsync("https://tenor.com/view/keanu-reeves-knife-gif-19576998");
+        }
+    }
+}
