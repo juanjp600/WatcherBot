daae9d41cafdbb4181ef350616e0f6f23da6d9c6
Jlobblet <jlobblet-github@gmx.co.uk>
2021-07-08 12:49:12 +0100

    Might be useful to have the config classes

---
+++ Bot600/Config/DiscordConfig.cs
@@ -0,0 +1,26 @@
+using System.Collections.Immutable;
+using System.Linq;
+using Discord.Rest;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+
+namespace Bot600
+{
+    public record DiscordConfig
+    {
+        public readonly ImmutableHashSet<RestRole> ModeratorRoles;
+        public readonly RestGuild OutputGuild;
+
+        public DiscordConfig(Config config, BaseSocketClient client)
+        {
+            RestGuild? outputGuild = client.Rest.GetGuildAsync(config.OutputGuildId).Result;
+            OutputGuild = outputGuild;
+            ModeratorRoles =
+                (from id in config.Configuration.GetSection("ModeratorRoles").Get<ulong[]>()
+                 let role = outputGuild.Roles.FirstOrDefault(r => r.Id == id)
+                 where role is not null
+                 select (RestRole) role)
+                .ToImmutableHashSet();
+        }
+    }
+}
