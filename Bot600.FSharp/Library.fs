551e2c23108c04de259e53b8c541fdbc5125ec6c
Jlobblet <jlobblet-github@gmx.co.uk>
2021-07-09 14:04:27 +0100

    Format code

--- Bot600.FSharp/Library.fs
+++ Bot600.FSharp/Library.fs
@@ -11,12 +11,14 @@ let parseHash (str: string) =
         /// Preceding character must not be part of a hash.
         /// Hash must be 5-40 characters.
         /// Optional `/` for URLs, then end of string.
-        let regex = Regex @"(?<![0-9a-fA-F])(?<hash>[0-9a-fA-F]{5,40})\/?$"
+        let regex =
+            Regex @"(?<![0-9a-fA-F])(?<hash>[0-9a-fA-F]{5,40})\/?$"
+
         match isNull input with
         | true -> None
         | false ->
             let m = regex.Match(input)
-            
+
             match m.Success with
             | true -> m.Groups.["hash"] |> Some
             | false -> None
@@ -24,28 +26,31 @@ let parseHash (str: string) =
     match str with
     | HashMatch hash -> hash |> fun g -> g.Value |> Ok
     | _ -> Error "Error executing !commitmsg: argument is invalid"
-    
-let deDupe (arr: Result<string, string>[]) =
+
+let deDupe (arr: Result<string, string> []) =
     let matches (s1: string) (s2: string) =
         s1.StartsWith(s2, StringComparison.OrdinalIgnoreCase)
         || s2.StartsWith(s1, StringComparison.OrdinalIgnoreCase)
-        
+
     let guard s1 acc =
-                acc |> List.exists (function
-                | Ok v2 -> matches s1 v2
-                | Error v2 -> matches s1 v2)
-    
+        acc
+        |> List.exists
+            (function
+            | Ok v2 -> matches s1 v2
+            | Error v2 -> matches s1 v2)
+
     let folder (elt: Result<string, string>) (acc: Result<string, string> list) =
         if
             match elt with
             | Ok v1
             | Error v1 when guard v1 acc -> true
             | _ -> false
-            then acc
-            else elt :: acc
-        
-    Array.foldBack folder arr []
-    |> Array.ofList
+        then
+            acc
+        else
+            elt :: acc
+
+    Array.foldBack folder arr [] |> Array.ofList
 
 /// hash is a Result<string, string> here so that it can be integrated with Async nicely
 let tryGetCommit (client: GitHubClient) hash =
@@ -84,8 +89,8 @@ let getCommitMessages client args =
     // Fetch everything at once
     |> Async.Parallel
     |> Async.RunSynchronously
-    |> Array.map (function
-            | Ok commit -> $"`{commit.Sha.Substring(0, 10)}: {commit.Commit.Message}`"
-            | Error message -> message
-    )
+    |> Array.map
+        (function
+        | Ok commit -> $"`{commit.Sha.Substring(0, 10)}: {commit.Commit.Message}`"
+        | Error message -> message)
     |> String.concat "\n"
