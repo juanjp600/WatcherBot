663ee63cce2834d941d976760a7b0954fd74df32
Juan Pablo Arce <juanjprot@gmail.com>
2021-08-02 11:14:45 -0300

    Add extra steps to DeployLinux.bat

--- DeployLinux.bat
+++ DeployLinux.bat
@@ -1,5 +1,7 @@
 @ECHO OFF
 
+dotnet tool restore
+dotnet ef database update --project Bot600\Bot600.csproj
 dotnet publish Bot600\Bot600.csproj -c Release -clp:ErrorsOnly;Summary --self-contained -r linux-arm
 
 PAUSE
