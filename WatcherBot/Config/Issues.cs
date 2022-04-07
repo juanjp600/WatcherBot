1f5ce834e537cfd30a3f490f0976b93e0fbadef1
Jlobblet <jlobblet-github@gmx.co.uk>
2022-04-07 23:53:44 +0100

    Ignore case in issue configuration

--- WatcherBot/Config/Issues.cs
+++ WatcherBot/Config/Issues.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using WatcherBot.Utils.ANSI;
 
@@ -5,12 +6,12 @@ namespace WatcherBot.Config;
 
 public class Issues
 {
-    private Dictionary<string, int> labelWeighting { get; } = new();
+    private Dictionary<string, int> labelWeighting { get; } = new(StringComparer.OrdinalIgnoreCase);
     public IReadOnlyDictionary<string, int> LabelWeighting => labelWeighting;
 
-    private HashSet<string> hideLabels { get; } = new();
+    private HashSet<string> hideLabels { get; } = new(StringComparer.OrdinalIgnoreCase);
     public IReadOnlySet<string> HideLabels => hideLabels;
 
-    private Dictionary<string, ForegroundColour> labelColours { get; } = new();
+    private Dictionary<string, ForegroundColour> labelColours { get; } = new(StringComparer.OrdinalIgnoreCase);
     public IReadOnlyDictionary<string, ForegroundColour> LabelColours => labelColours;
 }
