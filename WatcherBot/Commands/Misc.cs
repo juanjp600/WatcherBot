c3f288c2a1cd1108e5de26a199e7e03fe2a89303
Juan Pablo Arce <juanjproT@gmail.com>
2023-11-16 11:13:43 -0300

    Decommission hyena commands

---
+++ WatcherBot/Commands/Misc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using DisCatSharp.CommandsNext;
+using DisCatSharp.CommandsNext.Attributes;
+using WatcherBot.Utils;
+
+namespace WatcherBot.Commands;
+
+// ReSharper disable once UnusedType.Global
+[Hidden]
+public class MiscCommandModule : BaseCommandModule
+{
+    public MiscCommandModule() { }
+
+    [Command("trash")]
+    [Description("garbaggio")]
+    public Task Trash(CommandContext context) =>
+        throw new Exception("test exception");
+
+    [Command("sus")]
+    [Description("smh my head")]
+    public async Task Sus(CommandContext context) =>
+        await context.RespondDmAsync("https://tenor.com/view/keanu-reeves-knife-gif-19576998");
+
+    [Command("bezos")]
+    [Description("ffs")]
+    public async Task Bezos(CommandContext context) =>
+        await context.RespondDmAsync("please shut the fuck up");
+}
